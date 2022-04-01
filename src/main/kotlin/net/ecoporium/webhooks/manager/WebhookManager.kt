a89c63cf21a710d416cf42f7377971a1e7beaf8d
bobby29831 <bobby29831@gmail.com>
2022-04-01 14:41:21 -0500

    Add sending multiple embeds in the same message

--- src/main/kotlin/net/ecoporium/webhooks/manager/WebhookManager.kt
+++ src/main/kotlin/net/ecoporium/webhooks/manager/WebhookManager.kt
@@ -2,6 +2,7 @@ package net.ecoporium.webhooks.manager
 
 import club.minnced.discord.webhook.WebhookClient
 import club.minnced.discord.webhook.send.WebhookEmbed
+import club.minnced.discord.webhook.send.WebhookEmbedBuilder
 import club.minnced.discord.webhook.send.WebhookMessage
 import net.ecoporium.webhooks.SpartanWebhooks
 import java.io.IOException
@@ -24,9 +25,39 @@ class WebhookManager(private val plugin: SpartanWebhooks) {
 
     fun run(): () -> Unit = {
         if (embeds.isNotEmpty()) {
-            val embed = embeds.poll()
-            if (embed != null) send(WebhookMessage.embeds(embed))
+            val toAdd = mutableListOf<WebhookEmbed>()
+            for (i in 0 until 10) {
+                val embed = embeds.poll()
+                if (embed != null) {
+                    val title = embed.title
+                    if (title != null) {
+                        val url = if (title.url != null) "${title.url}?embed=${i}" else null
+                        toAdd.add(embed.builder().setTitle(WebhookEmbed.EmbedTitle(title.text, url)).build())
+                    } else {
+                        toAdd.add(embed)
+                    }
+                }
+                else break
+            }
+            send(WebhookMessage.embeds(toAdd))
         }
+    }
+
+    /**
+     * Copies all the fields from the given [WebhookEmbed] to a new embed, and returns a [WebhookEmbedBuilder] for it
+     */
+    private fun WebhookEmbed.builder(): WebhookEmbedBuilder {
+        val builder = WebhookEmbedBuilder()
+            .setTitle(this.title)
+            .setDescription(this.description)
+            .setAuthor(this.author)
+            .setColor(this.color)
+            .setFooter(this.footer)
+            .setImageUrl(this.imageUrl)
+            .setThumbnailUrl(this.thumbnailUrl)
+            .setTimestamp(this.timestamp)
+        this.fields.forEach { builder.addField(it) }
+        return builder
     }
 
 }
